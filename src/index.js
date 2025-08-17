6098c0f344d2e82303c0f02db4a8347b38312c2a
Maribelaya <shandermarina7@gmail.com>
2025-08-17 21:52:20 +0200

    HW7

--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@ const bootstrap = async () => {
 };
 
 void bootstrap();
+console.log('MONGO_USER:', process.env.MONGODB_USER);
+console.log('MONGO_PASSWORD:', process.env.MONGODB_PASSWORD);
+console.log('MONGO_URL:', process.env.MONGODB_URL);
+console.log('MONGO_DB:', process.env.MONGODB_DB);
